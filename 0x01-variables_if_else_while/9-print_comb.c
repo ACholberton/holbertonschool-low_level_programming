1ee4acfe1e28cdcf3da44d3ec9c9b772d216e20c
ACholberton <1103@holbertonschool.com>
2019-09-19 22:58:05 +0000

    added c programming file that will print a range of numbers separated by comas

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ *Return: always 0 (sucess)
+ */
+int main(void)
+{
+	int a = 0;
+
+	while (a != 10)
+	{
+		putchar(a + '0');
+		if (a != 9)
+		putchar(',');
+		putchar(' ');
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}

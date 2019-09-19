7374c71794731a814302d23f3fcafe137f95352d
ACholberton <1103@holbertonschool.com>
2019-09-19 21:04:48 +0000

    added c programming file that will print numbers from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - Entry Point
+ *
+ *Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int a = 0;
+	char NL = 10;
+
+	while (a < 10)
+	{
+		printf("%i", a);
+		a++;
+	}
+	putchar(NL);
+return (0);
+}

4cbfc330504fd0406e903d6dd9f81fd62a57c5cd
ACholberton <1103@holbertonschool.com>
2019-09-24 22:42:23 +0000

    added a function that displayed the multiplication table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,7 +16,7 @@ void times_table(void)
 		{
 			z = x * y;
 
-			if (z < 10 && x !=0)
+			if (z < 10 && x != 0)
 			{
 			_putchar(' ');
 			_putchar(' ');
@@ -27,7 +27,8 @@ void times_table(void)
 				_putchar(' ');
 				_putchar((z / 10) + '0');
 				_putchar((z % 10) + '0');
-	          	}
+			}
+
 			else
 				_putchar(z + '0');
 			if (x != 9)

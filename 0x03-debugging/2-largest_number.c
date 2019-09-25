9eab22157ce884b06669cde6e64294eddd5b50b6
ACholberton <1103@holbertonschool.com>
2019-09-25 02:02:08 +0000

    corrected errors in file and updated header file

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,31 +10,35 @@
 
 int largest_number(int a, int b, int c)
 {
-int largest;
-if (a > b && a > c)
-{
-largest = a;
-}
-else if (b > a && b > c)
-{
-largest = b;
-}
-else if (c > a && c > b)
-{
-largest = c;
-}
-else if ((a == b && a > c) || (a == c && a > b) || (b == c && a > b)
-{
-largest = a;
-}
-else if ((a == c && a < b) || (b == c && a < b))
-{
-largest = b;
-}
-else
-{
-largest = c;
-}
+	int largest;
 
-return (largest);
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (b > a && b > c)
+	{
+		largest = b;
+	}
+	else if (c > a && c > b)
+	{
+		largest = c;
+	}
+	else if ((a == b && a > c) || (a == c && a > b) || (b == c && a > b))
+	{
+		largest = a;
+	}
+	else if ((a == c && a < b) || (b == c && a < b))
+	{
+		largest = b;
+	}
+	else if (a == b && a < c)
+	{
+		largest = c;
+	}
+	else if (a == b && a == c)
+	{
+		largest = a;
+	}
+		return (largest);
 }

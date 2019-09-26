dd9ca5a9c033402d132bddcaf4db9d5858f9154d
ACholberton <1103@holbertonschool.com>
2019-09-26 17:50:54 +0000

    added fucntion that will print numbers from 0 to 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ *more_numbers - this function will print numbers from 0 to 14, 10 times
+ *Return: void
+ */
+void more_numbers(void)
+{
+	int a;
+	int b;
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+				_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}

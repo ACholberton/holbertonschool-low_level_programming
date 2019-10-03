8771bdccbe475bd473e759e4afc026ac045aab33
ACholberton <1103@holbertonschool.com>
2019-10-03 21:49:17 +0000

    added a function that will copy a string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ *_strcpy - this function will copy a string
+ *@dest: is a string
+ *@src: is a string
+ *@n: is avariable
+ *Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int code;
+
+	for (code = 0; src[code]; code++)
+	{
+		dest[code] = src[code];
+	}
+	for (; code < n; code++)
+	{
+		dest[code] = '\0';
+	}
+	return (dest);
+}

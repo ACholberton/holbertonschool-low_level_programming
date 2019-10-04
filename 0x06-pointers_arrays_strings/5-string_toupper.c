2cc71c78c6dc45714392c362491e1dc601e69d35
ACholberton <1103@holbertonschool.com>
2019-10-04 00:20:14 +0000

    added a function that will replace lower case letter with upper case

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *string_toupper - will write the letters of a string in upper case
+ *@UP: is a string
+ *Return: string
+ */
+char *string_toupper(char *UP)
+{
+	int a;
+
+	for (a = 0; UP[a] != '\0'; a++)
+	{
+		if (UP[a] >= 97 && UP[a] <= 123)
+		{
+			UP[a] = UP[a] -32;
+		}
+	}
+	return (UP);
+}

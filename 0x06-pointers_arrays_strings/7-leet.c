696b72c7d28e3432249ebea5fbd86844b8ead447
ACholberton <1103@holbertonschool.com>
2019-10-04 23:44:16 +0000

    added a function that will replace characters with numbers

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,10 +6,25 @@
  */
 char *leet(char *rep)
 {
-	int a, b, replace;
-replace 
-	for (a = 0; rep[a] != '\0'; a++)
+	int a, b;
+	char replace[5][3] = {{'a', 'A', '4'},
+			      {'e', 'E', '3'},
+			      {'o', 'O', '0'},
+			      {'t', 'T', '7'},
+			      {'l', 'L', '1'}};
+
+	for (a = 0; *(rep + a) != '\0';)
 	{
-		for (b = 0; replace[b] != '\0'; b++)
+		for (b = 0; b < 5; b++)
 		{
-			if ()
+			if (*(rep + a) == replace[b][0]
+			    || *(rep + a) == replace[b][1])
+			{
+				*(rep + a) = replace[b][2];
+				break;
+			}
+		}
+		a++;
+	}
+	return (rep);
+}

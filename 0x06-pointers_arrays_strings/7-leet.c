2abd67f00a6c1c15535a433aeb50c725c836a685
ACholberton <1103@holbertonschool.com>
2019-10-04 06:52:11 +0000

    added a function that will replace a character with number

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ *leet - encode the string into 1337
+ *@rep: is a string
+ *Return: string
+ */
+char *leet(char *rep)
+{
+	int a, b, replace;
+replace 
+	for (a = 0; rep[a] != '\0'; a++)
+	{
+		for (b = 0; replace[b] != '\0'; b++)
+		{
+			if ()

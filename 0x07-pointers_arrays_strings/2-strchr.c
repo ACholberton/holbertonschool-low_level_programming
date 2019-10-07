3322b9e722a838269b556f8f4f64dbc0b085a5ff
ACholberton <1103@holbertonschool.com>
2019-10-07 19:03:00 +0000

    added a function that will look for a specific character in a string

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strchr - will locate a character in a string
+ *@s: is a string
+ *@c: is a variable
+ *Return: string character
+ */
+char *_strchr(char *s, char c)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		if (s[a] == c)
+		{
+			return (s + a);
+		}
+		
+	}
+	return (0);
+}

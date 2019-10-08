7a2c7313fb53f2ce502a2ebf66e03e834ac8c7e3
ACholberton <1103@holbertonschool.com>
2019-10-08 00:16:12 +0000

    added a function that will locate the lenght of a substring

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ *_strstr - this function will locate a substring
+ *@haystack: is a string
+ *@needle: is a substring
+ *Return: string
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int a, b;
+
+	for (a = 0; haystack[a] != '\0'; a++)
+	{
+		for (b = 0; needle[b] != '\0'; b++)
+		{
+			if (haystack[a] == needle[b])
+			{
+				return (needle);
+			}
+		}
+	}
+	return ('\0');
+}

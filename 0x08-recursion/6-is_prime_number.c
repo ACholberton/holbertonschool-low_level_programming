c70a6a3ac81f56327fa49a7c4eccd8f27cfc39d8
ACholberton <1103@holbertonschool.com>
2019-10-10 01:50:11 +0000

    added a function that will return whether or not the integer is a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *is_prime_helper - is a helper function
+ *@i: is a variable
+ *Return: helper
+ */
+int is_prime_helper(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, i - 1));
+}
+#include "holberton.h"
+/**
+ *is_prime_number - will print 1 or 0 if it is a prime number
+ *@n: is a variable
+ *Return: 1
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+	return (is_prime_helper(n, n / 2));
+}

c7fbb798832811a94030f4b736f0bf84d955685f
ACholberton <1103@holbertonschool.com>
2019-10-13 01:56:12 +0000

    added a program that will print the number of arguments enteret to it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ *main - entry Point
+ *@argc: integer for arguments
+ *@argv: pointer to arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}

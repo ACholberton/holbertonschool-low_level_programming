fea582ccb5ec0f342e7efdea1b5ba9531ff68ab3
ACholberton <1103@holbertonschool.com>
2019-10-16 00:53:48 +0000

    added a function that concatenates 2 strings

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,60 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *_strlen - this function returns the lenght of  string
+ *@s: is a string
+ *Return: lenght
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+
+	while (*s)
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+/**
+ *str_concat - will concatenate two strings
+ *@s1: is a string
+ *@s2: is a string
+ *Return: Point in newly allocated space
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *point;
+	int x, y;
+	int s1length, s2length, s3sum;
+
+	if (s1 == '\0')
+		s1 = "";
+	if (s2 == '\0')
+		s2 = "";
+
+	s1length = _strlen(s1);
+	s2length = _strlen(s2);
+	s3sum = s2length + s1length;
+
+	point = (char *)malloc((sizeof(char) * (s3sum + 1)));
+
+	if (point == '\0')
+	{
+		return ('\0');
+	}
+
+	for (x = 0; s1[x]; x++)
+	{
+		point[x] = s1[x];
+	}
+	for (y = 0; s2[y]; y++)
+	{
+		point[x] = s2[y];
+	}
+	point[x] = '\0';
+	return (point);
+}

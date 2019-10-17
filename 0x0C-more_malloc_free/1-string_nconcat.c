d55f6a3806584243952fb870b2957c2c1fa23e50
ACholberton <1103@holbertonschool.com>
2019-10-17 22:33:03 +0000

    added a function that concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -32,9 +32,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int a, b;
 	unsigned int length1, length2, NEWL;
 
-	if (s1 == '\0')
+	if (s1 == NULL)
 		s1 = "";
-	if (s2 == '\0')
+	if (s2 == NULL)
 		s2 = "";
 
 	length1 = _strlen(s1);
@@ -47,7 +47,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	NEWL = length1 + length2;
 
-	point = (char *)malloc((sizeof(char) * (NEWL + 1)));
+	point = malloc((sizeof(char) * (NEWL + 1)));
 
 	if (point == NULL)
 	{

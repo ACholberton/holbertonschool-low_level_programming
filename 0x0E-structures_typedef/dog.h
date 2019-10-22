f0196be48a162523f1eeba495d1602f126cf1fcd
ACholberton <1103@holbertonschool.com>
2019-10-22 01:45:12 +0000

    corrected format and added _putchar

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ *struct dog - a structure
+ *@name:  is the name of the dog
+ *@age: is the age of the dog
+ *@owner: is the owner of the dog
+ */
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
+int _putchar(char c);
 #endif

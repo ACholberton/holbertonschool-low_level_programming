141e3f2664e36e0c657a5b1cc106198b46fba0cb
ACholberton <1103@holbertonschool.com>
2019-10-24 00:55:18 +0000

    added oparations file for task 3

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *op_add -  will make a sum
+ *@a: is an integer
+ *@b: is an integer
+ *Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - will make a substraction
+ *@a: is an integer
+ *@b: is an integer
+ *Return: sub
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul -  will make a multiplication
+ *@a: is an integer
+ *@b: is an integer
+ *Return: multiplication
+ */
+int op_mul(int a, int b);
+{
+	return (a * b);
+}
+/**
+ *op_div - will make a division
+ *@a: is an integer
+ *@b: is an integer
+ *Return: division
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ *op_mod -  returns a reminder
+ *@a: is an integer
+ *@b: is an integer
+ *Return: reminder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

d5f045e8e58b045d1781454c0dd5324d0a9162a2
ACholberton <1103@holbertonschool.com>
2019-10-25 00:28:55 +0000

    corrected order of arguments and eliminated indexes

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,35 +9,24 @@
  */
 int main(int argc, char *argv[])
 {
-	int total, a, b;
 	int (*point)(int, int);
 
-	if (argc < 4 || argv[1] == NULL || argv[3] == NULL)
+	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-
-	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
-	{
-		printf("Error\n");
-		exit(100);
-	}
-
-	a = atoi(argv[1]);
-	b = atoi(argv[3]);
-
 	point = get_op_func(argv[2]);
-
-	if (point == NULL || argv[2][1] != '\0')
+	if (point == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	total = point(a, b);
-
-	printf("%d\n", total);
-
+	if ((atoi(argv[3]) == 0) && (*argv[2] == '%' || *argv[2] == '/'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", point(atoi(argv[1]), atoi(argv[3])));
 	return (0);
 }

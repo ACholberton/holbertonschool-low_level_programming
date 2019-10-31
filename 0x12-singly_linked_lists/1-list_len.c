ca9f897d69b082921a526c0e26d45ab953927ec5
ACholberton <1103@holbertonschool.com>
2019-10-31 22:20:55 +0000

    added helper function

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,6 +2,24 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ *_strlen - this function returns the lenght of  string
+ *@s: is a string
+ *Return: lenght
+ */
+int _strlen(char *s)
+{
+	int l;
+
+	l = 0;
+
+	while (*s)
+	{
+		s++;
+		l++;
+	}
+	return (l);
+}
+/**
  *list_len - returns number of elements in lists
  *@h: the nodes
  *Return: number of elements

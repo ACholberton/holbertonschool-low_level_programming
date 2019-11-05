20fac007539e119e1cbd2afea359687614397249
ACholberton <1103@holbertonschool.com>
2019-11-05 01:31:12 +0000

    added a prototype function that will add a new node to the top of a list

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ *add_nodeint - will add a node to the beginning of a list
+ *@head: is the beginning of the list
+ *@n: is the node to be added
+ *Return: the address of the new element, or NULL if it failed
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *NN;
+
+	NN = malloc(sizeof(listint_t));
+	if (NN == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+	printf("%d\n", n);
+	NN->next = *head;
+	*head = NN;
+	NN->n = n;
+	return (NN);
+}

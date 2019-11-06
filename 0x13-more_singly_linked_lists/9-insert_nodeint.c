2cfe846722f03f43f87efc9ff179459a00452504
ACholberton <1103@holbertonschool.com>
2019-11-06 04:01:05 +0000

    moved index outside the loop

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -26,23 +26,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new->n = n;
 
-	if (idx == 0)
-	{
-		new->n = n;
-		new->next = *head;
-		*head = new;
-		return (new);
-	}
 	for (i = 0; tmp != NULL; i++)
 	{
-		/*if (idx == 0)
+		if (idx == 0)
 		{
 			new->n = n;
 			new->next = *head;
 			*head = new;
 			return (new);
-			}*/
-		if (i == (idx - 1))
+		}
+		else if (i == (idx - 1))
 		{
 			new->n = n;
 			new->next = tmp->next;

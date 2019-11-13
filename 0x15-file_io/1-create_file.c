4f46dc64e6f4ca2cc494ed3fca7d5d2a33d376f1
ACholberton <1103@holbertonschool.com>
2019-11-13 00:54:15 +0000

    added a function that creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+/**
+ *create_file - a function that creates a file.
+ *@filename: the file to be created
+ *@text_content: what is inside the file
+ *Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file, counter;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content == NULL)
+	{
+		text_content = "";
+	}
+	file = open(filename, O_CREAT | O_RDWR, 0600);
+	if (file < 0)
+	{
+		return (-1);
+	}
+
+	for (counter = 0; text_content[counter] != '\0'; counter++)
+	{
+		write(file, text_content, STDOUT_FILENO);
+	}
+	close(file);
+	return (1);
+}

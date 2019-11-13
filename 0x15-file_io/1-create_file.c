52698260a3078bec350e3a106515e5581cb02f17
ACholberton <1103@holbertonschool.com>
2019-11-13 01:00:31 +0000

    made changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,16 +17,15 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	file = open(filename, O_CREAT | O_RDWR, 0600, O_TRUNC);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file < 0)
 	{
 		return (-1);
 	}
 
 	for (counter = 0; text_content[counter] != '\0'; counter++)
-	{
-		write(file, text_content, STDOUT_FILENO);
-	}
+		;
+	write(file, text_content, counter);
 	close(file);
 	return (1);
 }

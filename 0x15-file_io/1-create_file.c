7995faca8a69ccd7724bb0a7f8a6935aca28ca1a
ACholberton <1103@holbertonschool.com>
2019-11-13 00:56:54 +0000

    added option for OPEN

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	file = open(filename, O_CREAT | O_RDWR, 0600);
+	file = open(filename, O_CREAT | O_RDWR, 0600, O_TRUNC);
 	if (file < 0)
 	{
 		return (-1);

140229c72bc7b33043838fbcf87088b58482a277
ACholberton <1103@holbertonschool.com>
2020-04-16 21:48:39 +0000

    fixed betty style coding

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -5,7 +5,7 @@
  *Binary search algorithm
  *@array: is the array
  *@size: is the size fo the array
- *@value is the value to be found
+ *@value: is the value to be found
  *Return: value, if not present return -1
  */
 
@@ -22,12 +22,12 @@ int binary_search(int *array, size_t size, int value)
 	while (index_l <= index_r)
 	{
 		printf("Searching in array: 0, 1, 2, 3, 4, 5, 6, 7, 8, 9\n");
-		mid = ((index_l + index_r) /2 );
+		mid = ((index_l + index_r) / 2);
 		if (array[mid] < value)
 		{
 			index_l = mid + 1;
 		}
-		else if(array[mid] > value)
+		else if (array[mid] > value)
 		{
 			index_r = mid - 1;
 		}
